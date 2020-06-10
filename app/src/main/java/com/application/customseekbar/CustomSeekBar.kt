cdd8cc3ef9204b2536a33d079c132c8b4ef7625e
Anica <developer.anica@gmail.com>
2020-06-10 22:27:22 +0200

    Fixed number hiding behind padding issue.

--- app/src/main/java/com/application/customseekbar/CustomSeekBar.kt
+++ app/src/main/java/com/application/customseekbar/CustomSeekBar.kt
@@ -1,41 +1,40 @@
 package com.application.customseekbar
 
 import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.os.Bundle
 import android.util.AttributeSet
 import android.widget.SeekBar
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+
 
 @SuppressLint("AppCompatCustomView")
 class CustomSeekBar : SeekBar {
     private val paint: Paint = Paint()
 
     constructor(context: Context?) : super(context) {}
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {}
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
-        context,
-        attrs,
-        defStyleAttr
-    ) {
-    }
+    constructor(context: Context?, attrs: AttributeSet?) :
+            super(context, attrs) {}
+    constructor(context: Context?, attrs: AttributeSet?,
+                defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
 
     @Synchronized
     override fun onDraw(canvas: Canvas) {
-        /**these are fractions:
-         * getProgress()        getWidth()
-         *     ---        *       ---       =   55.2 (in our case)
-         *   getMax()              1
-         */
         val thumb =
-            this.progress.toDouble() / this.max * getWidth().toDouble()
+            (this.progress.toDouble() / this.max.toDouble()) *
+                    (getWidth().toDouble() - (paddingEnd.toDouble()+paddingStart.toDouble()))
 
         paint.setColor(Color.RED)
         paint.setTextSize(50f)
         canvas.drawText(
             progress.toString(),
-            convertToFloat(thumb) - 70,
+            convertToFloat(thumb),
             60f,
             paint
         )
--- app/src/main/java/com/application/customseekbar/MainActivity.kt
+++ app/src/main/java/com/application/customseekbar/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.application.customseekbar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,25 +11,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        customSeekBar.setPadding(0, 0, 0, 0)
+        customSeekBar.setPadding(30, 0, 40, 0)
         customSeekBar.setProgress(1)
 
-        customSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
-                if (customSeekBar.getProgress() == 0) {
-                    customSeekBar.setProgress(1)
+        customSeekBar.setOnSeekBarChangeListener(
+            object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(
+                    seekBar: SeekBar,
+                    i: Int,
+                    b: Boolean) {
+                    if (customSeekBar.getProgress() == 0) {
+                        customSeekBar.setProgress(1)
+                    }
                 }
-            }
 
-            override fun onStartTrackingTouch(seekBar: SeekBar) {
-                if (customSeekBar.getProgress() == 0) {
-                    customSeekBar.setProgress(1)
+                override fun onStartTrackingTouch(seekBar: SeekBar) {
+                    if (customSeekBar.getProgress() == 0) {
+                        customSeekBar.setProgress(1)
+                    }
                 }
-            }
 
-            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                override fun onStopTrackingTouch(seekBar: SeekBar) {
 
-            }
-        })
+                }
+            })
     }
 }
